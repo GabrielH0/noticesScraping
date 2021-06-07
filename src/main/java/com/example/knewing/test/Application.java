547bb02f3c8cc616b03d11a602df425515b7fcfd
GabrielH0 <gabrilghb10@hotmail.com>
2021-06-07 00:29:28 -0300

    [009] remover imports desnecessarios

--- src/main/java/com/example/knewing/test/Application.java
+++ src/main/java/com/example/knewing/test/Application.java
@@ -1,18 +1,12 @@
 package com.example.knewing.test;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
